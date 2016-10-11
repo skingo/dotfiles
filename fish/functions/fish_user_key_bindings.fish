9290f77d6d7fb585e3b43d9e58110bc1074caedb
skinge <ingo.skupin@gmail.com>
2016-10-12 00:15:35 +0200

    resolve key bindings problem with omf

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,13 @@
-
-#vi_mode_insert
-#fish_vi_mode
-fish_vi_key_bindings
-bind -M insert \cg end-of-line
-bind -M insert \ce end-of-line
-bind -M insert \cx beginning-of-line
-bind -M insert \ca beginning-of-line
-bind -M insert \cy yank
-bind -e -M insert \cd
-bind -e \cd
-fzf_key_bindings
+function fish_user_key_bindings
+	#vi_mode_insert
+	#fish_vi_mode
+	fish_vi_key_bindings
+	bind -M insert \cg end-of-line
+	bind -M insert \ce end-of-line
+	bind -M insert \cx beginning-of-line
+	bind -M insert \ca beginning-of-line
+	bind -M insert \cy yank
+	bind -e -M insert \cd
+	bind -e \cd
+	fzf_key_bindings
+end
