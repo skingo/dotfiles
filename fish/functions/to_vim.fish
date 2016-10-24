1e9f58ace1c95e7a909dc663d28b08dd2d895c2b
skinge <ingo.skupin@gmail.com>
2016-10-24 15:21:22 +0200

    wsi account, tweak to vim recognition

--- fish/functions/to_vim.fish
+++ fish/functions/to_vim.fish
@@ -1,8 +1,8 @@
 function to_vim
 	if in_vim_session 
-echo "exiting session"
-exit
-else
-echo "not in a vim session"
-end
+		echo "exiting session"
+		exit
+	else
+		echo "not in a vim session"
+	end
 end
