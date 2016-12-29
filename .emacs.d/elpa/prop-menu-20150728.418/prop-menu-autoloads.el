7315db60c3566270c20d5c5431895b06023946eb
skinge <ingo.skupin@gmail.com>
2016-12-30 00:26:11 +0100

    cleanup (i.e. remove) emacs.d

--- .emacs.d/elpa/prop-menu-20150728.418/prop-menu-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; prop-menu-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("prop-menu.el") (22118 54901 810640 104000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; prop-menu-autoloads.el ends here
